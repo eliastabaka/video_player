d9293b7236ec7b6e7a43777b92042c7c6f579b08
Milosz Tabaka <miloszmtabaka@gmail.com>
2023-01-22 11:55:21 +0000

    Initial Commit

---
+++ video_player/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  video_player
+//
+//  Created by Elias Tabaka on 22/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
